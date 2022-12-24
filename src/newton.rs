87b520b52e8d536599bd766595be19282565ce6e
shota-tsuji <shotatuzi@gmail.com>
2022-12-24 15:17:51 +0900

    feat: change newton-method interface to accept fn pointer

--- src/newton.rs
+++ src/newton.rs
@@ -1,11 +1,15 @@
 use ndarray::{arr1, Array1, ArrayView1};
 
-pub fn newton(x_0: ArrayView1<f64>, f_x: fn(f64) -> f64, f_xx: fn(f64) -> f64) -> Array1<f64> {
+pub fn newton(
+    x_0: ArrayView1<f64>,
+    del_f: Vec<Box<impl Fn(f64) -> f64>>,
+    f_xx: fn(f64) -> f64,
+) -> Array1<f64> {
     let eps = 1e-10;
     let mut x_k_ = x_0.to_owned();
     let mut x_k1 = 0.0;
     loop {
-        x_k1 = x_k_[0] - f_x(x_k_[0]) / f_xx(x_k_[0]);
+        x_k1 = x_k_[0] - (*del_f[0])(x_k_[0]) / f_xx(x_k_[0]);
         if (x_k1 - x_k_[0]).abs() < eps {
             break;
         }
@@ -27,10 +31,11 @@ mod tests {
         let eps = 1e-10;
         let x_0 = arr1(&[0.0]);
         let f_x = |x: f64| 2.0 * x - 2.0;
+        let del_f = vec![Box::new(f_x)];
         let f_xx = |x: f64| 2.0;
         let h = vec![vec![f_xx]];
         let ans = arr1(&[1.0]);
-        let x_k1 = newton(x_0.view(), f_x, f_xx);
+        let x_k1 = newton(x_0.view(), del_f, f_xx);
         assert!(norm_l2((ans - x_k1).view()) < eps);
     }
 
@@ -40,9 +45,10 @@ mod tests {
         let eps = 1e-10;
         let x_0 = arr1(&[0.0]);
         let f_x = |x: f64| 2.0 * x - 4.0;
+        let del_f = vec![Box::new(f_x)];
         let f_xx = |x: f64| 2.0;
         let ans = arr1(&[2.0]);
-        let x_k1 = newton(x_0.view(), f_x, f_xx);
+        let x_k1 = newton(x_0.view(), del_f, f_xx);
         assert!(norm_l2((ans - x_k1).view()) < eps);
     }
 
@@ -52,9 +58,10 @@ mod tests {
         let eps = 1e-10;
         let x_0 = arr1(&[2.0]);
         let f_x = |x: f64| 3.0 * x * x + -4.0 * x + 1.0;
+        let del_f = vec![Box::new(f_x)];
         let f_xx = |x: f64| 6.0 * x - 4.0;
         let ans = arr1(&[1.0]);
-        let x_k1 = newton(x_0.view(), f_x, f_xx);
+        let x_k1 = newton(x_0.view(), del_f, f_xx);
         assert!(norm_l2((ans - x_k1).view()) < eps);
     }
 
