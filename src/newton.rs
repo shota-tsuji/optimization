bf91ef44cb3ab867dc2a301a3c3a67d08d169bb6
shota-tsuji <shotatuzi@gmail.com>
2022-12-24 11:53:40 +0900

    test: add x^3 equation test to newton-method

--- src/newton.rs
+++ src/newton.rs
@@ -1,4 +1,4 @@
-pub fn newton(x_0: f64, f_x: fn(f64)-> f64, f_xx: fn(f64)-> f64, eps: f64) -> f64 {
+pub fn newton(x_0: f64, f_x: fn(f64) -> f64, f_xx: fn(f64) -> f64, eps: f64) -> f64 {
     let mut x_k_ = x_0;
     let mut x_k1 = 0.0;
     loop {
@@ -22,7 +22,7 @@ mod tests {
         // f(x) = x^2 - 2x + 1
         let eps = 1e-10;
         let x_0 = 0.0;
-        let f_x = |x: f64| 2.0*x - 2.0;
+        let f_x = |x: f64| 2.0 * x - 2.0;
         let f_xx = |x: f64| 2.0;
         assert!((1.0 - newton(x_0, f_x, f_xx, eps)).abs() < eps);
     }
@@ -32,8 +32,18 @@ mod tests {
         // f(x) = x^2 -4x +4
         let eps = 1e-10;
         let x_0 = 0.0;
-        let f_x = |x: f64| 2.0*x - 4.0;
+        let f_x = |x: f64| 2.0 * x - 4.0;
         let f_xx = |x: f64| 2.0;
         assert!((2.0 - newton(x_0, f_x, f_xx, eps)).abs() < eps);
+    }
+
+    #[test]
+    fn newton_1x1_power3() {
+        // f(x) = x^3 -2x^3 + x + 3
+        let eps = 1e-10;
+        let x_0 = 2.0;
+        let f_x = |x: f64| 3.0 * x * x + -4.0 * x + 1.0;
+        let f_xx = |x: f64| 6.0 * x - 4.0;
+        assert!((1.0 - newton(x_0, f_x, f_xx, eps)).abs() < eps);
     }
 }
