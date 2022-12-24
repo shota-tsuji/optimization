380a9f096b2dc1f4e1caecf977ae76e1725606a6
shota-tsuji <shotatuzi@gmail.com>
2022-12-24 20:04:02 +0900

    feat: use array to iterate hessian and right-hand vector b

--- src/newton.rs
+++ src/newton.rs
@@ -1,23 +1,42 @@
-use ndarray::{arr1, Array1, ArrayView1};
+use ndarray::{arr1, Array1, Array2, ArrayView1, Dim};
 
 pub fn newton(
     x_0: ArrayView1<f64>,
-    del_f: Vec<impl Fn(f64) -> f64>,
-    f_xx: fn(f64) -> f64,
+    del_f: Vec<impl Fn(ArrayView1<f64>) -> f64>,
+    h: Vec<Vec<impl Fn(ArrayView1<f64>) -> f64>>,
 ) -> Array1<f64> {
-    let eps = 1e-10;
+    let delta = 1e-10;
     let mut x_k_ = x_0.to_owned();
-    let mut x_k1 = 0.0;
+    let mut x_k1 = Array1::zeros(x_k_.raw_dim());
+    let n = x_0.len();
+    let mut a = Array2::zeros((n, n));
+    let mut b = Array1::zeros((n));
     loop {
-        x_k1 = x_k_[0] - del_f[0](x_k_[0]) / f_xx(x_k_[0]);
-        if (x_k1 - x_k_[0]).abs() < eps {
+        for i in 0..n {
+            for j in 0..n {
+                a[[i, j]] = h[i][j](x_k_.view());
+            }
+        }
+        println!("a={:?}", a);
+
+        for i in 0..n {
+            b[[i]] = -del_f[i](x_k_.view());
+        }
+        println!("b={:?}", b);
+
+        x_k1[0] = x_k_[0] + b[[0]] / a[[0, 0]];
+        if norm_l2((&x_k1 - &x_k_).view()) < delta {
             break;
         }
-        x_k_[[0]] = x_k1;
-        println!("{}", x_k1)
+        x_k_[[0]] = x_k1[0];
+        println!("x={}", x_k1)
     }
 
-    arr1(&[x_k1])
+    x_k1
+}
+
+fn norm_l2(x: ArrayView1<f64>) -> f64 {
+    x.map(|x| x * x).sum().sqrt()
 }
 
 #[cfg(test)]
@@ -30,12 +49,12 @@ mod tests {
         // f(x) = x^2 - 2x + 1
         let eps = 1e-10;
         let x_0 = arr1(&[0.0]);
-        let f_x = |x: f64| 2.0 * x - 2.0;
+        let f_x = |x: ArrayView1<f64>| 2.0 * x[[0]] - 2.0;
         let del_f = vec![&f_x];
-        let f_xx = |x: f64| 2.0;
-        let h = vec![vec![f_xx]];
+        let f_xx = |x: ArrayView1<f64>| 2.0;
+        let h = vec![vec![&f_xx]];
         let ans = arr1(&[1.0]);
-        let x_k1 = newton(x_0.view(), del_f, f_xx);
+        let x_k1 = newton(x_0.view(), del_f, h);
         assert!(norm_l2((ans - x_k1).view()) < eps);
     }
 
@@ -44,11 +63,12 @@ mod tests {
         // f(x) = x^2 -4x +4
         let eps = 1e-10;
         let x_0 = arr1(&[0.0]);
-        let f_x = |x: f64| 2.0 * x - 4.0;
+        let f_x = |x: ArrayView1<f64>| 2.0 * x[[0]] - 4.0;
         let del_f = vec![&f_x];
-        let f_xx = |x: f64| 2.0;
+        let f_xx = |x: ArrayView1<f64>| 2.0;
+        let h = vec![vec![&f_xx]];
         let ans = arr1(&[2.0]);
-        let x_k1 = newton(x_0.view(), del_f, f_xx);
+        let x_k1 = newton(x_0.view(), del_f, h);
         assert!(norm_l2((ans - x_k1).view()) < eps);
     }
 
@@ -57,15 +77,12 @@ mod tests {
         // f(x) = x^3 -2x^3 + x + 3
         let eps = 1e-10;
         let x_0 = arr1(&[2.0]);
-        let f_x = |x: f64| 3.0 * x * x + -4.0 * x + 1.0;
+        let f_x = |x: ArrayView1<f64>| 3.0 * x[[0]] * x[[0]] - 4.0 * x[[0]] + 1.0;
         let del_f = vec![&f_x];
-        let f_xx = |x: f64| 6.0 * x - 4.0;
+        let f_xx = |x: ArrayView1<f64>| 6.0 * x[[0]] - 4.0;
+        let h = vec![vec![&f_xx]];
         let ans = arr1(&[1.0]);
-        let x_k1 = newton(x_0.view(), del_f, f_xx);
+        let x_k1 = newton(x_0.view(), del_f, h);
         assert!(norm_l2((ans - x_k1).view()) < eps);
-    }
-
-    fn norm_l2(x: ArrayView1<f64>) -> f64 {
-        x.map(|x| x * x).sum().sqrt()
     }
 }
