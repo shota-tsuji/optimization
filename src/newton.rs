c362f66f1f9ecbfc470e57efb1641f4ca3a09af9
shota-tsuji <shotatuzi@gmail.com>
2022-12-24 15:22:05 +0900

    refactor: remove Box from newton-method

--- src/newton.rs
+++ src/newton.rs
@@ -2,14 +2,14 @@ use ndarray::{arr1, Array1, ArrayView1};
 
 pub fn newton(
     x_0: ArrayView1<f64>,
-    del_f: Vec<Box<impl Fn(f64) -> f64>>,
+    del_f: Vec<impl Fn(f64) -> f64>,
     f_xx: fn(f64) -> f64,
 ) -> Array1<f64> {
     let eps = 1e-10;
     let mut x_k_ = x_0.to_owned();
     let mut x_k1 = 0.0;
     loop {
-        x_k1 = x_k_[0] - (*del_f[0])(x_k_[0]) / f_xx(x_k_[0]);
+        x_k1 = x_k_[0] - del_f[0](x_k_[0]) / f_xx(x_k_[0]);
         if (x_k1 - x_k_[0]).abs() < eps {
             break;
         }
@@ -31,7 +31,7 @@ mod tests {
         let eps = 1e-10;
         let x_0 = arr1(&[0.0]);
         let f_x = |x: f64| 2.0 * x - 2.0;
-        let del_f = vec![Box::new(f_x)];
+        let del_f = vec![&f_x];
         let f_xx = |x: f64| 2.0;
         let h = vec![vec![f_xx]];
         let ans = arr1(&[1.0]);
@@ -45,7 +45,7 @@ mod tests {
         let eps = 1e-10;
         let x_0 = arr1(&[0.0]);
         let f_x = |x: f64| 2.0 * x - 4.0;
-        let del_f = vec![Box::new(f_x)];
+        let del_f = vec![&f_x];
         let f_xx = |x: f64| 2.0;
         let ans = arr1(&[2.0]);
         let x_k1 = newton(x_0.view(), del_f, f_xx);
@@ -58,7 +58,7 @@ mod tests {
         let eps = 1e-10;
         let x_0 = arr1(&[2.0]);
         let f_x = |x: f64| 3.0 * x * x + -4.0 * x + 1.0;
-        let del_f = vec![Box::new(f_x)];
+        let del_f = vec![&f_x];
         let f_xx = |x: f64| 6.0 * x - 4.0;
         let ans = arr1(&[1.0]);
         let x_k1 = newton(x_0.view(), del_f, f_xx);
