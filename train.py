60720643c87a150c7e88c6a2ccf9d42ab4ff5348
shota-tsuji <shotatuzi@gmail.com>
2023-01-22 09:40:58 +0900

    fix: conduct binary classification {0, 1} instead of {-1, 1}

--- train.py
+++ train.py
@@ -7,9 +7,9 @@ from scipy.optimize import fmin_bfgs
 def main():
     df = pd.read_csv('a1a.csv', header=None)
     labels = df[0]
-    #print(labels)
+    pos_class = labels.unique()[1]
+    mask = labels == pos_class
     data = df.iloc[:, 1:]
-    #print(data)
     # number of feature parameters
     n = 123
     # number of train data
@@ -19,21 +19,23 @@ def main():
     for (i, row) in data.iterrows():
         for j in row:
             features[i, j-1] = 1.0
+    y_bin = np.ones(labels.shape, dtype=features.dtype)
+    y_bin[~mask] = 0.0
+    print(y_bin)
 
     w = np.zeros(n)
-    print(f(w, labels, features))
-    print(phi(w, labels, features))
-    print(la.norm(phi(w, labels, features)))
+    print(f(w, y_bin, features))
+    print(la.norm(phi(w, y_bin, features)))
 
-    x0 = np.array([-3, -4])
-    cost_weight = np.diag([1., 10.])
-    #fmin_bfgs(quadratic_cost, x0, args=(cost_weight,))
+    (xopt, fopt, gopt, _, _, _, _) = fmin_bfgs(f, w, phi, args=(y_bin, features), maxiter=100, full_output=True)
+    print(fopt)
+    print(gopt)
 
 def f(w, y, features):
     sum = 0.0
     i = 0
     for x in features:
-        sum += math.log(1.0 + math.exp(- y[i] * w @ x))
+        sum += (math.log(1.0 + math.exp(w @ x)) - y[i] * w @ x)
         i += 1
 
     return sum
@@ -46,7 +48,10 @@ def phi(w, y, features):
 
     for x in features:
         for j in range(n):
-            g[j] += - y[i] * features[i, j] / (1.0 + math.exp(y[i] * w @ x))
+            #g[j] += - y[i] * features[i, j] / (1.0 + math.exp(y[i] * w @ x))
+            #p = math.exp(w @ x) / 1 + math.exp(w @ x)
+            p = 1.0 / (1.0 + math.exp(- w @ x))
+            g[j] += features[i, j] * (p - y[i])
         i += 1
 
     return g
